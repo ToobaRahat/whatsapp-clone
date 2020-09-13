8bc64f67e04c5fd9a400070d8f61cc1162ac025f
Tooba <tooba.rahatali@gmail.com>
2020-09-13 14:07:44 +0500

    add all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
+import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const pusher = new Pusher("76c0bcb7ac16a2794dd2", {
+      cluster: "eu",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (newMessage) => {
+      alert(JSON.stringify(newMessage));
+      setMessages([...messages, newMessage]);
+    });
+    // cleanup function so that the loop ends somewhere
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__body">
+        {/* sidebar */}
+        <Sidebar />
+
+        {/* chat component */}
+        <Chat messages={messages} />
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD-8oRRs3Iwu1Gr7-dh6Lwk7CmTi0rKECQ",
+  authDomain: "whatsapp-clone-8d89e.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-8d89e.firebaseio.com",
+  projectId: "whatsapp-clone-8d89e",
+  storageBucket: "whatsapp-clone-8d89e.appspot.com",
+  messagingSenderId: "268345243376",
+  appId: "1:268345243376:web:60ecca674ce3533acb7435",
+};
